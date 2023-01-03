14e252e5dfb07db7e3c038911392ae511d8451e7
Oskari Järvelin <contact@oskarijarvelin.fi>
2023-01-03 12:16:09 +0200

    Added return arrow

--- components/header.tsx
+++ components/header.tsx
@@ -2,7 +2,8 @@ import Link from 'next/link'
 
 export default function Header() {
   return (
-    <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
+    <h2 className="flex align-center text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
+      <span className="mr-2" style={{ transform: 'translateY(-2px)' }}>&larr; </span>
       <Link href="/" className="hover:underline">
         Blogi
       </Link>
