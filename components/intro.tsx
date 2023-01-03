3bb547d97ded639f463dac023285e11d02839d49
Oskari Järvelin <contact@oskarijarvelin.fi>
2023-01-03 11:54:18 +0200

    Updated intro spacing

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,7 +1,7 @@
 export default function Intro() {
   return (
-    <section className="flex-col md:flex-row flex items-end md:justify-between mt-16 mb-16 lg:mb-32">
-      <h1 className="text-8xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+    <section className="flex-col md:flex-row flex items-end justify-center md:justify-between mt-16 mb-16 lg:mb-32">
+      <h1 className="text-8xl md:text-8xl font-bold tracking-tighter leading-tight mb:2 md:mb-0 md:pr-8">
         Blogi.
       </h1>
       <p className="text-center md:text-left text-lg mb-3 md:pl-8">
