5a8bb5bcf04a8a5f58f5bdfbe69c219e2010eab5
Oskari Järvelin <contact@oskarijarvelin.fi>
2023-01-03 11:51:34 +0200

    Updated intro spacing

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,11 +1,11 @@
 export default function Intro() {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 lg:mb-32">
+    <section className="flex-col md:flex-row flex items-end md:justify-between mt-16 mb-16 lg:mb-32">
       <h1 className="text-8xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         Blogi.
       </h1>
-      <p className="text-center md:text-left text-lg mt-5 md:pl-8">
-        Web-kehittäjän, yrittäjän ja tapahtuma-alan monitoimimiehen blogi.
+      <p className="text-center md:text-left text-lg mb-3 md:pl-8">
+        Web-kehitys &#10025; Saavutettavuus &#10025; Yrittäjyys &#10025; Tapahtumatekniikka &#10025; Podcastit
       </p>
     </section>
   )
--- components/post-title.tsx
+++ components/post-title.tsx
@@ -1,7 +1,7 @@
 export default function PostTitle({ children }) {
   return (
     <h1
-      className="text-6xl md:text-7xl lg:text-8xl font-bold tracking-tighter leading-tight md:leading-none mb-0 md:mb-12 text-center md:text-left"
+      className="text-6xl md:text-7xl lg:text-8xl font-bold tracking-tighter leading-tight md:leading-none mb-0 md:mb-12"
       dangerouslySetInnerHTML={{ __html: children }}
     />
   )
