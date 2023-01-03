0319f163c181a3ccce5547b15f847e8fc3391ed7
Oskari Järvelin <contact@oskarijarvelin.fi>
2023-01-03 11:41:32 +0200

    Updated spacings

--- components/hero-post.tsx
+++ components/hero-post.tsx
@@ -18,7 +18,7 @@ export default function HeroPost({
           <CoverImage title={title} coverImage={coverImage} slug={slug} />
         )}
       </div>
-      <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
+      <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-36 md:mb-48">
         <div>
           <h2 className="mb-4 md:mb-8 text-4xl lg:text-6xl font-bold leading-tight">
             <Link
